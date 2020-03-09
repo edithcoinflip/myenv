441ff31bcad9bbe6bb98ec0e0cd87b77fd7c5500
Adam Edelstein <Adam.Edelstein@tastytrade.com>
2020-03-09 09:35:54 -0500

    fishupdates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,9 @@ export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
 #export EDITOR=emacs
 
 #test -x /usr/bin/lesspipe; and eval "$(SHELL=/bin/sh lesspipe)"
+
+source ~/.iterm2_shell_integration.fish
+function iterm2_print_user_vars
+    set -l git_branch (git branch 2> /dev/null | sed -n '/\* /s///p')
+    iterm2_set_user_var gitBranch "$git_branch"
+end
